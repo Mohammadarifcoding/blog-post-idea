15345ec178258fd54f71107cb06fde98ac442ec6
Mohammad Arif <programmermdarif@gmail.com>
2025-07-26 18:17:48 +0600

    updated the setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { QueryClientProvider } from '@tanstack/svelte-query';
+
+	import { QueryClient } from '@tanstack/query-core';
+	let { children } = $props();
+	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
+
+	const queryClient = new QueryClient();
+</script>
+
+<QueryClientProvider client={queryClient}>
+	<Navbar />
+	{@render children?.()}
+</QueryClientProvider>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<script>
+	// import Home from '../components/page/Home/Home.svelte';
+</script>
